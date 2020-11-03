7aaffb80713fe4649672c5dedcc6afab696c1960
Surbhi Gupta <surbhi.gupt@gmail.com>
2020-11-03 18:58:52 -0500

    Create Minimum Swaps 2.py

---
+++ Hackerrank/Interview Prep/Arrays/Minimum Swaps 2.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    swaps = 0
+    n = len(arr)
+ 
+    for idx in range(n):
+        while arr[idx]-1 != idx:
+            ele = arr[idx]
+            arr[ele-1], arr[idx] = arr[idx], arr[ele-1]
+            swaps += 1
+    return swaps
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
