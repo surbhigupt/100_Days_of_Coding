6e6cc532acc24f81481ebcfd0898ce7efe44ad98
Surbhi Gupta <surbhi.gupt@gmail.com>
2020-12-01 11:55:49 -0500

    Create Frequency Queries .py

---
+++ Hackerrank/Interview Prep/Dictionaries and Hashmaps/Frequency Queries .py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import defaultdict
+
+# Complete the freqQuery function below.
+def freqQuery(queries):
+    
+    results = []
+    lookup = dict()
+    freqs = defaultdict(set)
+    for command, value in queries:
+        freq = lookup.get(value, 0)
+        if command == 1:
+            lookup[value] = freq + 1
+            freqs[freq].discard(value)
+            freqs[freq + 1].add(value)
+        elif command == 2:
+            lookup[value] = max(0, freq - 1)
+            freqs[freq].discard(value)
+            freqs[freq - 1].add(value)
+        elif command == 3:
+            results.append(1 if freqs[value] else 0)
+    return results  
+                
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    ans = freqQuery(queries)
+
+    fptr.write('\n'.join(map(str, ans)))
+    fptr.write('\n')
+
+    fptr.close()
